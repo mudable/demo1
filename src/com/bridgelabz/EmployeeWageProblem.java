6d9c827050fede2ed75dd6d7737e53843a61d77b
mudable <mudableshilpa@gmail.com>
2021-09-08 09:49:54 +0530

    well come to employeewage prolem

---
+++ src/com/bridgelabz/EmployeeWageProblem.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmployeeWageProblem {
+    public static void main(String[] args) {
+        System.out.println("Well come to employeewage problem ");
+    }
+}
