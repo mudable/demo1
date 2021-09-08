15f3a914e1367e73293043dbca41e167238a7421
mudable <mudableshilpa@gmail.com>
2021-09-08 10:05:44 +0530

    ADDED: Check Employee Is Present Or Not.

--- src/com/bridgelabz/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmployeeWageProblem.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
 
 public class EmployeeWageProblem {
+    public static int isPresent(){
+        return (int) (Math.floor(Math.random()*10)%2);
+    }
+
     public static void main(String[] args) {
         System.out.println("Well come to employeewage problem ");
+        int IS_FULL_TIME=1;
+
+        if(IS_FULL_TIME == isPresent())
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
+
     }
 }
